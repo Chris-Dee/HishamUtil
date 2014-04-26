7a56f636910c2f2e7d00a0fbbdc284fe1a45fd8c
sam-ginsburg <sbg11@duke.edu>
2014-04-26 02:12:39 -0400

    FUCKIN CHRIS DONT UNDERSTAND RECURSIVE SHIT FUCK

--- src/MOSFET.java
+++ src/MOSFET.java
@@ -11,7 +11,7 @@ public class MOSFET {
 	
 	public static double vtn0(double Na,double Xox, double QfQit){
 		double result = Vtn(0, Na, Xox, QfQit);
-		System.out.print("The MOSFET threshold voltage at V_BS=0 is: ");
+		System.out.print("The MOSFET threshold voltage at V_BS=0 is (V): ");
 		return C.s(result);
 	}
 	
@@ -22,9 +22,33 @@ public class MOSFET {
 	}
 	
 	public static double VDsat(double VGS, double VBS, double Na,double Xox, double QfQit){
-		double result = VGS - Vtn(VBS, Na, Xox, QfQit);
-		System.out.print("The drain voltage at saturation VDsat is: ");
+		double vtn = Vtn(VBS, Na, Xox, QfQit);
+		double result = VGS - vtn;
+		if(VGS<=vtn){
+			System.out.println("BTW, this NMOSFET is OFF and I_D_off = 0 A.");
+		}
+		else{
+			System.out.println("BTW, this NMOSFET is ON, If V_DS < VDsat, linear range, else saturation range.");
+		}
+		System.out.print("The drain voltage at saturation VDsat is (V): ");
 		return C.s(result);
+	}
+	
+	public static double Kn(double mu, double W, double L, double Xox){
+		double result = mu * MOSCAP.Cox(Xox) * (W/L);
+		System.out.print("K_n for the MOSFET is: ");
+		return C.s(result);
+	}
+	
+	public static double IDlin(double Kn, double VDS, double VGS, double VBS, double Na,double Xox, double QfQit){
+		double vtn = Vtn(VBS, Na, Xox, QfQit);
+		double result = Kn*(((VGS-vtn)*VDS) - 0.5*(C.p(VDS, 2)));
+		System.out.print("The drain current in the linear range for the MOSFET is (A): ");
+		return C.s(result);
+	}
+	
+	public static double IDsat(){
+		return 0;
 	}
 	
 }
--- src/Runner.java
+++ src/Runner.java
@@ -5,7 +5,8 @@ public class Runner {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println(PNJunctions.Vbi(500*C.EE(15), 400*C.EE(17)));
+		//System.out.println(PNJunctions.Vbi(500*C.EE(15), 400*C.EE(17)));
+		System.out.println(MOSFET.IDlin(5, 3, 6, 7, 8, 9, 10));
 
 	}
 
