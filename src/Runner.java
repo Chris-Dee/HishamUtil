2fb9fbd59fab87672b457bd4269376ec82962900
sam-ginsburg <sbg11@duke.edu>
2014-04-26 03:41:58 -0400

    ok im tired

--- src/Runner.java
+++ src/Runner.java
@@ -5,10 +5,10 @@ public class Runner {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//System.out.println(PNJunctions.Vbi(500*C.EE(15), 400*C.EE(17)));
-		System.out.println(MOSFET.IDlin(5, 3, 6, 7, 8, 9, 10));
-		System.out.println("yay "+MOSCAP.Vtn(1.2*C.EE(18),3.2*C.EE(10)*C.q,22*C.EE(-8)));
-		//System.out.println(PNJunctions.Vbi(500*C.EE(15), 400*C.EE(17)));
+		PNJunctions.Vbi(500*C.EE(15), 400*C.EE(17));
+		//MOSFET.IDlin(5, 3, 6, 7, 8, 9, 10);
+		MOSCAP.Vtn(1.2*C.EE(18),3.2*C.EE(10)*C.q,22*C.EE(-8));
+		PNJunctions.Vbi(500*C.EE(15), 400*C.EE(17));
 	}
 
 }
