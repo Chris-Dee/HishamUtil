d009240ba505a961c3303794a29dd8de458ccb42
Chris-Dee <cmd45@duke.edu>
2014-04-26 00:29:03 -0400

    This one's for Hisham

--- src/InversionBias.java
+++ src/InversionBias.java
@@ -2,24 +2,30 @@
 public class InversionBias extends MOSCAP {
 
 	public static double Qdep(double Na){
-		return -Math.sqrt(4*C.q*C.esi*Na*phiFb(Na));
+		System.out.print("Depletion region charge: ");
+		return C.s( -Math.sqrt(4*C.q*C.esi*Na*phiFb(Na)));
 	}
 	public static double Qsc(double Na, double QfQit, double Xox, double Vgs){
-		return -Cox(Vgs-2*phiFb(Na)-Vfb(Na,Xox,QfQit));
+		System.out.print("Space charge: ");
+		return C.s( -Cox(Vgs-2*phiFb(Na)-Vfb(Na,Xox,QfQit)));
 	}
 //	public static double Qinv(double Na, double QfQit,double Xox,double Vgs){
 //		return Qsc(Na,QfQit,Xox,Vgs)+Math.sqrt(4*C.q*C.esi*Na*phiFb(Na));
 //	}
 	public static double Vox(double Vgs,double Na){
-		return Vgs-2*phiFb(Na)-PhiPm(Na);
+		System.out.print("Voltage across the oxide: ");
+		return C.s( Vgs-2*phiFb(Na)-PhiPm(Na));
 	}
 	public static double Eox(double Xox,double Vgs,double Na){
-		return Vox(Vgs,Na)/Xox;
+		System.out.print("Field across the oxide: ");
+		return C.s(Vox(Vgs,Na)/Xox);
 	}
 	public static double Qg(double Xox,double Na,double Vgs){
-		return Cox(Xox)*(Vgs-2*phiFb(Na)-PhiPm(Na));
+		System.out.print("Charge on the gate: ");
+		return C.s( Cox(Xox)*(Vgs-2*phiFb(Na)-PhiPm(Na)));
 	}
 	public static double Qinv(double Xox,double Vgs,double QfQit, double Na){
-		return -Cox(Xox)*(Vgs-Vtn(Na,QfQit,Xox));
+		System.out.print("Charge in the inversion region: ");
+		return C.s(-Cox(Xox)*(Vgs-Vtn(Na,QfQit,Xox)));
 	}
 	}
--- src/ThresholdMOSCAP.java
+++ src/ThresholdMOSCAP.java
@@ -1,24 +1,31 @@
 
 public class ThresholdMOSCAP extends MOSCAP {
 	public static double Vtn(double Na,double QfQit,double Xox){
-		return Vfb(Na,Xox,QfQit)+2*phiFb(Na)+Math.sqrt(2*C.q*C.esi*Na*phiFb(Na))/Cox(Xox);
+		System.out.print("Threshold Voltage: ");
+		return C.s( Vfb(Na,Xox,QfQit)+2*phiFb(Na)+Math.sqrt(2*C.q*C.esi*Na*phiFb(Na))/Cox(Xox));
 	}
 	public static double VSC(double Na){
-		return 2*phiFb(Na);
+		System.out.print("Space charge Voltage: ");
+		return C.s( 2*phiFb(Na));
 	}
 	public static double Wd (double Na){
-		return Math.sqrt(4*C.esi*phiFb(Na)/(C.q*Na));
+		System.out.print("Width of the depletion region: ");
+		return C.s( Math.sqrt(4*C.esi*phiFb(Na)/(C.q*Na)));
 	}
 	public static double Qsc(double Na){
-		return -Math.sqrt(4*C.q*C.esi*Na*phiFb(Na));
+		System.out.print("Space charge: ");
+		return C.s( -Math.sqrt(4*C.q*C.esi*Na*phiFb(Na)));
 	}
 	public static double Qg(double Na,double QfQit){
-		return Math.sqrt(4*C.q*C.esi*Na*phiFb(Na))-QfQit;
+		System.out.print("Charge on the gate: ");
+		return C.s( Math.sqrt(4*C.q*C.esi*Na*phiFb(Na))-QfQit);
 	}
 	public static double Eox(double Na, double QfQit){
-		return Qg(Na,QfQit)/C.eox;
+		System.out.print("Oxide Field: ");
+		return C.s( Qg(Na,QfQit)/C.eox);
 	}
 	public static double Vox(double Xox, double Na, double QfQit){
-		return Eox(Na,QfQit)*Xox;
+		System.out.print("Voltage on the oxide: ");
+		return C.s( Eox(Na,QfQit)*Xox);
 	}
 }
