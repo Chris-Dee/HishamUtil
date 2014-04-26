cac4e3c9b6661f40b6fd99a223605032e5005f01
LingJesse <ling.jesse@gmail.com>
2014-04-25 22:10:20 -0400

    Commented Constants
    Added Comments and Units to Constants

--- src/C.java
+++ src/C.java
@@ -1,20 +1,21 @@
 
 public class C {
 
-public static double q=1.612177*EE(-19);	
-public static double kb=8.617385*EE(-5);
-public static double mo=9.909389*EE(-28);
-public static double eo=8.854187*EE(-14);
-public static double esi=11.7*eo;
-public static double eox=3.9*eo;
-public static double Navo=6.022136*EE(23);
-public static double eV=1.602177*EE(-19);
-public static double h=6.626*EE(-34); //in joules. slide 1.5
-public static double hbar=1.054572*EE(-34);
-public static double kbtq=0.02585;
-public static double latticeSi=5.4037; //in angstroms. rest are on 1.6
-public static double ni=1.07*EE(10);
-public static double mndos=1.090;
+public static double q=1.612177*EE(-19); //Electron Charge (C)
+public static double kb=8.617385*EE(-5); //Boltzmann's Constant (eV/K)
+public static double mo=9.909389*EE(-28); //Electron Rest Mass (g)
+public static double eo=8.854187*EE(-14); //Dielectric Permittibity of Vacuum (F/cm)
+public static double esi=11.7*eo; //Dielectric Permeability of Silicon (F/cm)
+public static double eox=3.9*eo; //Dielectric Permeability of Silicon Dioxide (F/cm)
+public static double Navo=6.022136*EE(23); //Avogadro's Number (mole-1)
+public static double eV=1.602177*EE(-19); //Electron Volt (J)
+public static double h=6.626*EE(-34); //Plank's Constant in joules*s. slide 1.5
+public static double hbar=1.054572*EE(-34); //Reduced Plank's Constant (J*s)
+public static double kbtq=0.02585; //(kvT/q) (V) 
+public static double latticeSi=5.4037; //Lattice Consant in angstroms. rest are on 1.7
+public static double ni=1.07*EE(10); //Intrinsic Carrier Concentration (cm-3)
+public static double mndos=1.090; //Electron Density of States Effective Mass
+
 public static double exp(double arg){
 	return p(Math.E,arg);
 }
