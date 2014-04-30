006ba5586f6da92d2279850edaa73298a1b24748
LingJesse <ling.jesse@gmail.com>
2014-04-30 00:41:27 -0400

    Final?

--- src/PNJunctions.java
+++ src/PNJunctions.java
@@ -49,21 +49,16 @@ public class PNJunctions {
 		System.out.print("Electrons at Depletion Region: ");
 		return C.s(Npo*C.exp((Vpn)/C.kbtq));
 	}
-	
+
 	/**PN-Junction Diode Current Density (12 review 2) */
 	//Add this 
 	//Add diode current 
-	
+
 	/**Small Signal PN-Junction Diode Depletion Capacitance Per Unit Area */
 	public static double depletionCapacitance(double Na, double Nd, double Vpn){
 		System.out.print("Depletion region capacitance value: ");
 		return C.s( Math.sqrt((C.q*C.esi*Na*Nd)/(2*(Na+Nd)*(Vbi(Na,Nd)-Vpn))));
 	}
-	
-	/**Small Signal Diffusion Capacitance On N-Side Per Unit Area */
-	public static double nCapacitance(double Nd, double Vpn){
-		System.out.print("N-Side Region Capacitance Value: ");
-		return C.s((C.p(C.q, 2)*C.p(C.ni,2)*Physics.L_pN(D_pN, tau_recN))/(C.kb*C.t*Nd)*C.exp((C.q*Vpn)/(C.kb*C.t)));
-	}
+
 	//other capacitances on 22. Get a lotta ugly stuff in there
 }
